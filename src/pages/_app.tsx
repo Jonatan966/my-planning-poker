eba54d326a46a506ad0a8b06439360a19a31ebf2
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-08-25 21:01:24 -0300

    feat: implement room context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
+import { RoomContextProvider } from "../contexts/room-context";
 
 import "../styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RoomContextProvider>
+      <Head>
+        <title>My Planning Poker</title>
+      </Head>
       <Toaster />
       <Component {...pageProps} />
-    </>
+    </RoomContextProvider>
   );
 }
