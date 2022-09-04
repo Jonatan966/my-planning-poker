31527b15aabd4141311b7954060ce7e69eb7e38a
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-09-04 17:31:46 -0300

    feat: remove useless room function

--- src/pages/rooms/[room_id].tsx
+++ src/pages/rooms/[room_id].tsx
@@ -6,37 +6,10 @@ import RoomHeader from "../../components/domain/room-header";
 import Table from "../../components/domain/table";
 
 import styles from "../../styles/pages/room.module.css";
-import { useRoom } from "../../contexts/room-context";
-import { api } from "../../lib/axios";
 import ConnectionDialog from "../../components/domain/connection-dialog";
 
 function RoomPage() {
-  const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
-
-  const { peerId } = useRoom();
-
-  useEffect(() => {
-    const roomId = router.query.room_id as string;
-
-    function onWindowClose() {
-      navigator.sendBeacon(
-        `${api.defaults.baseURL}/events/people-leave`,
-        JSON.stringify({
-          room_id: roomId,
-          people_id: peerId,
-        })
-      );
-    }
-
-    window.addEventListener("beforeunload", onWindowClose);
-    window.addEventListener("popstate", onWindowClose);
-
-    return () => {
-      window.removeEventListener("beforeunload", onWindowClose);
-      window.removeEventListener("popstate", onWindowClose);
-    };
-  }, [router, peerId]);
 
   return (
     <>
