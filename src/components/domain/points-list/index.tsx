c4fb6a66e1487fa08270dca7605e2037de696407
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-09-19 19:48:04 -0300

    fix: fire unanimous confetti only in correct time

--- src/components/domain/points-list/index.tsx
+++ src/components/domain/points-list/index.tsx
@@ -107,7 +107,9 @@ function PointsList() {
 
     const { average, isUnanimous } = calculatePointsAverage(peoples);
 
-    fireUnanimousConfetti();
+    if (isUnanimous) {
+      fireUnanimousConfetti();
+    }
 
     return (
       <div
