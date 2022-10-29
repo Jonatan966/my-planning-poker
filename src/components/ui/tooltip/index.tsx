8899881baffcc51269ab766a070ceea35cb4677f
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-10-29 12:05:02 -0300

    feat: use radix slot component into tooltip

--- src/components/ui/tooltip/index.tsx
+++ src/components/ui/tooltip/index.tsx
@@ -5,25 +5,28 @@ import React, {
   useId,
 } from "react";
 import ReactTooltip, { TooltipProps } from "react-tooltip";
+import { Slot } from "@radix-ui/react-slot";
 
 import styles from "./styles.module.css";
 
 interface CustomTooltipProps extends TooltipProps {
   message: string;
   children: ReactNode;
+  asChild?: boolean;
 }
 
 const TooltipComponent: ForwardRefRenderFunction<
   ReactTooltip,
   CustomTooltipProps
-> = ({ children, message, ...tooltipProps }, ref) => {
+> = ({ children, message, asChild, ...tooltipProps }, ref) => {
+  const Wrapper = asChild ? Slot : "div";
   const tooltipId = useId();
 
   return (
     <React.Fragment>
-      <div data-tip data-for={tooltipId} className={styles.tooltipTrigger}>
+      <Wrapper data-tip data-for={tooltipId} className={styles.tooltipTrigger}>
         {children}
-      </div>
+      </Wrapper>
       <ReactTooltip
         place="bottom"
         effect="solid"
