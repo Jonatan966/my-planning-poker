a0ccc23e3bc9558e627a0531f5f7765a0e88356b
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-12-09 20:04:52 -0300

    feat: create room access context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,20 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 import { ConfettiProvider } from "../contexts/confetti-context";
+import { RoomAccessProvider } from "../contexts/room-access-context";
 
 import "../styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ConfettiProvider>
-      <Head>
-        <title>My Planning Poker</title>
-      </Head>
-      <Toaster />
-      <Component {...pageProps} />
+      <RoomAccessProvider>
+        <Head>
+          <title>My Planning Poker</title>
+        </Head>
+        <Toaster />
+        <Component {...pageProps} />
+      </RoomAccessProvider>
     </ConfettiProvider>
   );
 }
