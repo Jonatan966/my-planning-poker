1049fbd37cae3546ae34bc12bacf1296c43fb7b9
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-02-03 20:46:03 -0300

    feat: config next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
+
 import { ConfettiProvider } from "../contexts/confetti-context";
 import { RoomAccessProvider } from "../contexts/room-access-context";
 
@@ -8,14 +10,16 @@ import "../styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ConfettiProvider>
-      <RoomAccessProvider>
-        <Head>
-          <title>My Planning Poker</title>
-        </Head>
-        <Toaster />
-        <Component {...pageProps} />
-      </RoomAccessProvider>
-    </ConfettiProvider>
+    <SessionProvider session={pageProps.session}>
+      <ConfettiProvider>
+        <RoomAccessProvider>
+          <Head>
+            <title>My Planning Poker</title>
+          </Head>
+          <Toaster />
+          <Component {...pageProps} />
+        </RoomAccessProvider>
+      </ConfettiProvider>
+    </SessionProvider>
   );
 }
