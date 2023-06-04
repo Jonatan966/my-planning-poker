a4fb59fbfe0642f56401bc4da7a43371c601dac7
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-06-04 15:31:24 -0300

    feat: add dev credits to project

--- src/configs/app.ts
+++ src/configs/app.ts
@@ -1,4 +1,6 @@
 export const appConfig = {
   isDevelopment: process.env.VERCEL_ENV === "development",
   environment: process.env.VERCEL_ENV || process.env.NEXT_PUBLIC_VERCEL_ENV,
+  repositoryUrl: "https://github.com/Jonatan966/my-planning-poker",
+  creatorUrl: "https://github.com/Jonatan966",
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { FaInfoCircle, FaNewspaper } from "react-icons/fa";
+import { FaGithub, FaInfoCircle } from "react-icons/fa";
 import { ConnectionForm } from "../components/domain/connection-form";
 import { FeedbackDialog } from "../components/domain/feedback-dialog";
 import { QuickRoomAccess } from "../components/domain/quick-room-access";
 import Button from "../components/ui/button";
 
+import { DevCredits } from "../components/domain/dev-credits";
 import { TabMenu } from "../components/ui/tab-menu";
 import { Tooltip } from "../components/ui/tooltip";
+import { appConfig } from "../configs/app";
 import { errorCodes } from "../configs/error-codes";
 import styles from "../styles/pages/home.module.css";
 
@@ -34,49 +36,61 @@ function HomePage() {
   }, [router.isReady]);
 
   return (
-    <div className={styles.container}>
-      <section className={styles.contentBox}>
-        <div className={styles.appName}>
-          <img src="/favicon.png" alt="Logotipo da aplicação" />
-          <h3 translate="no">My Planning Poker</h3>
+    <>
+      <div className={styles.container}>
+        <section className={styles.contentBox}>
+          <div className={styles.appName}>
+            <img src="/favicon.png" alt="Logotipo da aplicação" />
+            <h3 translate="no">My Planning Poker</h3>
 
-          <div className={styles.menu}>
-            <FeedbackDialog />
+            <div className={styles.menu}>
+              <FeedbackDialog />
 
-            <Button colorScheme="secondary" title="Changelog" disabled>
-              <FaNewspaper size={18} />
-            </Button>
+              <Tooltip message="Ir para repositório do projeto">
+                <a
+                  href={appConfig.repositoryUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Repositório do projeto"
+                >
+                  <Button colorScheme="secondary">
+                    <FaGithub size={18} />
+                  </Button>
+                </a>
+              </Tooltip>
+            </div>
           </div>
-        </div>
 
-        <TabMenu
-          disabled={isLoading}
-          menus={[
-            {
-              id: "enter",
-              name: "Entrar na sala",
-            },
-            {
-              id: "create",
-              name: "Criar sala",
-            },
-          ]}
-          selectedMenu={menu}
-          setSelectedMenu={setMenu}
-        >
-          <Tooltip message="Entre em uma sala existente ou crie uma nova">
-            <FaInfoCircle size={20} />
-          </Tooltip>
-        </TabMenu>
-        <ConnectionForm
-          menu={menu}
-          setIsLoading={setIsLoading}
-          isLoading={isLoading}
-        >
-          <QuickRoomAccess />
-        </ConnectionForm>
-      </section>
-    </div>
+          <TabMenu
+            disabled={isLoading}
+            menus={[
+              {
+                id: "enter",
+                name: "Entrar na sala",
+              },
+              {
+                id: "create",
+                name: "Criar sala",
+              },
+            ]}
+            selectedMenu={menu}
+            setSelectedMenu={setMenu}
+          >
+            <Tooltip message="Entre em uma sala existente ou crie uma nova">
+              <FaInfoCircle size={20} />
+            </Tooltip>
+          </TabMenu>
+          <ConnectionForm
+            menu={menu}
+            setIsLoading={setIsLoading}
+            isLoading={isLoading}
+          >
+            <QuickRoomAccess />
+          </ConnectionForm>
+        </section>
+      </div>
+      <DevCredits />
+    </>
   );
 }
 
